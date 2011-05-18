802973bfe1f5b7fad50062178c1687cfaf350789
Kerner <alex.kerner.24@googlemail.com>
2011-05-18 16:03:29 +0200

    [Feature] Introduce ListTransformer.

---
+++ src/main/java/net/sf/kerner/utils/collections/list/ListTransformer.java
@@ -0,0 +1,7 @@
+package net.sf.kerner.utils.collections.list;
+
+import net.sf.kerner.utils.collections.CollectionTransformer;
+
+public interface ListTransformer<T, V> extends CollectionTransformer<T,V> {
+
+}
