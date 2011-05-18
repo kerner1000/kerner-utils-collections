fbfb2d3a3718dc27e618476204e51138bd1f086f
Kerner <alex.kerner.24@googlemail.com>
2011-05-18 16:06:11 +0200

    Revert "[Feature] Introduce ListTransformer."
    This reverts commit 802973bfe1f5b7fad50062178c1687cfaf350789.

--- src/main/java/net/sf/kerner/utils/collections/list/ListTransformer.java
+++
@@ -1,7 +0,0 @@
-package net.sf.kerner.utils.collections.list;
-
-import net.sf.kerner.utils.collections.CollectionTransformer;
-
-public interface ListTransformer<T, V> extends CollectionTransformer<T,V> {
-
-}
