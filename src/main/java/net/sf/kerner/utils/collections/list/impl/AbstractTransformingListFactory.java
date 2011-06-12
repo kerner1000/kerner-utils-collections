41ccc1a0ecffc6a618629d4b0398b677b737d2b7
Kerner <alex.kerner.24@googlemail.com>
2011-06-12 22:19:14 +0200

    Stuff.
    Change-Id: If01c3c18dcd780af93a8509d93b26f96efea239c

--- src/main/java/net/sf/kerner/utils/collections/list/impl/AbstractTransformingListFactory.java
+++ src/main/java/net/sf/kerner/utils/collections/list/impl/AbstractTransformingListFactory.java
@@ -1,20 +1,38 @@
 package net.sf.kerner.utils.collections.list.impl;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import net.sf.kerner.utils.collections.list.TransformingListFactory;
 import net.sf.kerner.utils.transformer.Transformer;
 
-public abstract class AbstractTransformingListFactory<S, E> implements TransformingListFactory<S, E>, Transformer<S, E>{
-	
+public abstract class AbstractTransformingListFactory<S, E> implements
+		TransformingListFactory<S, E>, Transformer<S, E> {
+
+	private final List<? extends S> template;
+
+	public AbstractTransformingListFactory() {
+		this.template = null;
+	}
+
+	public AbstractTransformingListFactory(List<? extends S> template) {
+		this.template = template;
+	}
+
+	public List<E> createCollection() {
+		return createCollection(template);
+	}
+
 	public List<E> createCollection(Collection<? extends S> elements) {
-		final List<E> result = new ArrayList<E>();
-		for(S s : elements){
-			result.add(transform(s));
+		final List<E> result = getListImpl();
+		if (elements != null) {
+			for (S s : elements) {
+				result.add(transform(s));
+			}
 		}
 		return result;
 	}
+	
+	protected abstract List<E> getListImpl();
 
 }
