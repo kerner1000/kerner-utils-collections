f369000933cc678cb2fc90a10a9a71e44dd6175f
Kerner <alex.kerner.24@googlemail.com>
2011-06-19 23:17:01 +0200

    [Feature] Introduce CollectionUtils#toString().

--- src/main/java/net/sf/kerner/utils/collections/CollectionUtils.java
+++ src/main/java/net/sf/kerner/utils/collections/CollectionUtils.java
@@ -2,10 +2,25 @@ package net.sf.kerner.utils.collections;
 
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.Iterator;
+
+import net.sf.kerner.utils.StringUtils;
 
 public class CollectionUtils {
 	
 	private CollectionUtils(){}
+	
+	public static String toString(Iterable<?> elements){
+		final StringBuilder b = new StringBuilder();
+		b.append(StringUtils.NEW_LINE_STRING);
+		final Iterator<?> i = elements.iterator();
+		while(i.hasNext()){
+			b.append(i.next());
+			if(i.hasNext())
+				b.append(StringUtils.NEW_LINE_STRING);
+		}
+		return b.toString();
+	}
 	
 	public static <T> T getHighest(Collection<? extends T> elements, Comparator<T> c){
 		T result = null;
