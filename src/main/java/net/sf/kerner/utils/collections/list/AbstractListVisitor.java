6fcdc483fb9c006835d685b2d3d81ccd5eb98d0a
Kerner <alex.kerner.24@googlemail.com>
2011-09-30 13:48:43 +0200

    [Feature] AbstractListVisitor now supports filtering.

--- src/main/java/net/sf/kerner/utils/collections/list/AbstractListVisitor.java
+++ src/main/java/net/sf/kerner/utils/collections/list/AbstractListVisitor.java
@@ -1,10 +1,23 @@
 package net.sf.kerner.utils.collections.list;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import net.sf.kerner.utils.collections.DefaultVisitor;
+import net.sf.kerner.utils.collections.Filter;
 
 public abstract class AbstractListVisitor<E> implements ListWalker<E>, DefaultVisitor<E> {
+	
+	protected final Collection<Filter<E>> filters = new ArrayList<Filter<E>>();
+	
+	public synchronized void addFilter(Filter<E> filter){
+		filters.add(filter);
+	}
+	
+	public synchronized void clearFilters(){
+		filters.clear();
+	}
 	
 	public void beforeWalk(){
 		
@@ -13,6 +26,16 @@ public abstract class AbstractListVisitor<E> implements ListWalker<E>, DefaultVi
 	public synchronized void walk(List<? extends E> list) {
 		beforeWalk();
 		for(E e : list){
+			boolean take = true;
+			for(Filter<E> f : filters){
+				if(f.visit(e)){
+					// take
+				} else {
+					take = false;
+					break;
+				}
+			}
+			if(take)
 			visit(e);
 		}
 		afterWalk();
