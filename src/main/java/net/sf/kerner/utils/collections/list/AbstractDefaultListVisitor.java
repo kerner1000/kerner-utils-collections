17eeb19cefa2f05c7af7af927aa5192a76e2f3cd
Kerner <alex.kerner.24@googlemail.com>
2011-09-30 15:41:14 +0200

    [API-Break] AbstractListTransformer now extends ListVisitor.

--- src/main/java/net/sf/kerner/utils/collections/list/AbstractDefaultListVisitor.java
+++ src/main/java/net/sf/kerner/utils/collections/list/AbstractDefaultListVisitor.java
@@ -1,5 +1,10 @@
 package net.sf.kerner.utils.collections.list;
 
 public abstract class AbstractDefaultListVisitor<E> extends AbstractListVisitor<Void, E>{
+	
+	@Override
+	protected void handleVisit(Void v) {
+		// nothing		
+	}
 
 }
--- src/main/java/net/sf/kerner/utils/collections/list/AbstractListTransformer.java
+++ src/main/java/net/sf/kerner/utils/collections/list/AbstractListTransformer.java
@@ -5,32 +5,42 @@ import java.util.List;
 import net.sf.kerner.utils.collections.list.impl.ArrayListFactory;
 import net.sf.kerner.utils.transformer.Transformer;
 
-public abstract class AbstractListTransformer<T, V> implements Transformer<T, V>,
-		ListTransformer<T, V> {
+public abstract class AbstractListTransformer<T, V> extends AbstractListVisitor<V, T> implements
+		Transformer<T, V>, ListTransformer<T, V> {
 
 	protected final ListFactory<V> factory;
+
+	protected volatile List<V> result;
 
 	public AbstractListTransformer(ListFactory<V> factory) {
 		super();
 		this.factory = factory;
 	}
-	
+
 	public AbstractListTransformer() {
 		this(new ArrayListFactory<V>());
 	}
+
+	@Override
+	public void beforeWalk() {
+		super.beforeWalk();
+		result = factory.createCollection();
+	}
+	
+	protected void handleVisit(V v) {
+		result.add(v);
+	};
+
+	public V visit(T element) {
+		return transform(element);
+	};
 
 	/**
 	 * if {@code element == null}, empty list is returned.
 	 */
 	public synchronized List<V> transformList(List<? extends T> element) {
-		final List<V> result = factory.createCollection();
-		if(element != null)
-		for (T t : element) {
-//			if(t == null)
-//				result.add(null);
-//			else
-			result.add(transform(t));
-		}
+		if (element != null)
+			walk(element);
 		return result;
 	}
 
