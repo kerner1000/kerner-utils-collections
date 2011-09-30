ae326560dadab92c127ee1cc3260beef0ae31910
Kerner <alex.kerner.24@googlemail.com>
2011-09-30 18:36:03 +0200

    [API-Break] ListVisitor: get ListIterator instead of index.

--- src/main/java/net/sf/kerner/utils/collections/list/AbstractListVisitor.java
+++ src/main/java/net/sf/kerner/utils/collections/list/AbstractListVisitor.java
@@ -3,48 +3,59 @@ package net.sf.kerner.utils.collections.list;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.ListIterator;
 
 import net.sf.kerner.utils.collections.Filter;
 import net.sf.kerner.utils.collections.Visitor;
 
-public abstract class AbstractListVisitor<V, E> implements ListWalker<E>, Visitor<V, E> {
-	
+public abstract class AbstractListVisitor<V, E> implements ListWalker<E>, ListVisitor<V, E> {
+
 	protected final Collection<Filter<E>> filters = new ArrayList<Filter<E>>();
-	
-	public synchronized void addFilter(Filter<E> filter){
+
+	protected volatile ListIterator<? extends E> iterator;
+
+	public synchronized void addFilter(Filter<E> filter) {
 		filters.add(filter);
 	}
-	
-	public synchronized void clearFilters(){
+
+	public synchronized void clearFilters() {
 		filters.clear();
 	}
-	
-	public void beforeWalk(){
-		
+
+	public void beforeWalk() {
+
 	}
-	
-	public synchronized void walk(List<? extends E> list) {
-		beforeWalk();
-		for(E e : list){
-			boolean take = true;
-			for(Filter<E> f : filters){
-				if(f.visit(e)){
-					// take
-				} else {
-					take = false;
-					break;
-				}
+
+	public void walk(List<? extends E> list) {
+		iterator = list.listIterator();
+		synchronized (iterator) {
+			beforeWalk();
+			while (iterator.hasNext()) {
+					final E e = iterator.next();
+					boolean take = true;
+					for (Filter<E> f : filters) {
+						if (f.visit(e)) {
+							// take
+						} else {
+							take = false;
+							break;
+						}
+					}
+					if (take)
+						handleVisit(visit(e));
 			}
-			if(take)
-			handleVisit(visit(e));
+			afterWalk();
 		}
-		afterWalk();
+	}
+
+	public void afterWalk() {
+
 	}
 	
-	public void afterWalk(){
-		
+	public ListIterator<? extends E> getIterator() {
+		return iterator;
 	}
-	
+
 	protected abstract void handleVisit(V v);
 
 }
--- src/main/java/net/sf/kerner/utils/collections/list/ListVisitor.java
+++ src/main/java/net/sf/kerner/utils/collections/list/ListVisitor.java
@@ -1,9 +1,11 @@
 package net.sf.kerner.utils.collections.list;
 
+import java.util.ListIterator;
+
 import net.sf.kerner.utils.collections.Visitor;
 
 public interface ListVisitor<V, E> extends Visitor<V, E> {
 	
-	int getIndex();
+	ListIterator<? extends E> getIterator();
 
 }
