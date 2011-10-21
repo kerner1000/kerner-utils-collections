60bcbfb79102e24f14158bf18b3677bc10cfb212
Kerner <alex.kerner.24@googlemail.com>
2011-10-21 09:57:02 +0200

    [Feature] Introduce SortedMapFactory.

---
+++ src/main/java/net/sf/kerner/utils/collections/map/SortedMapFactory.java
@@ -0,0 +1,9 @@
+package net.sf.kerner.utils.collections.map;
+
+import java.util.SortedMap;
+
+import net.sf.kerner.utils.factory.Factory;
+
+public interface SortedMapFactory<K, V> extends Factory<SortedMap<K, V>>{
+
+}
