5fe1b2901e0447a3edba2d3968df224266ac4c89
Kerner <alex.kerner.24@googlemail.com>
2011-10-21 09:56:29 +0200

    CleanUp.

--- src/main/java/net/sf/kerner/utils/collections/map/MapUtils.java
+++ src/main/java/net/sf/kerner/utils/collections/map/MapUtils.java
@@ -20,11 +20,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.SortedMap;
 
 import net.sf.kerner.utils.Utils;
 import net.sf.kerner.utils.collections.CollectionFactory;
