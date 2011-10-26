e2ba60f5fb69fdb6aa830014833c588c9a35a269
Kerner <alex.kerner.24@googlemail.com>
2011-10-26 17:03:01 +0200

    Test stuff.

---
+++ src/test/java/net/sf/kerner/utils/collections/TestFilterInverter.java
@@ -0,0 +1,67 @@
+package net.sf.kerner.utils.collections;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestFilterInverter {
+	
+	private FilterInverter<Boolean> fi;
+	
+	private Filter<Boolean> f;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testFilterInverter01() {
+		f = new Filter<Boolean>(){
+			public Boolean visit(Boolean element) {
+				return element;
+			}};
+		fi = new FilterInverter<Boolean>(f);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public final void testFilterInverter02() {
+		fi = new FilterInverter<Boolean>(null);
+	}
+
+	@Test
+	public final void testVisit01() {
+		f = new Filter<Boolean>(){
+			public Boolean visit(Boolean element) {
+				return element;
+			}};
+		fi = new FilterInverter<Boolean>(f);
+		assertFalse(fi.visit(Boolean.TRUE));
+	}
+	
+	@Test
+	public final void testVisit02() {
+		f = new Filter<Boolean>(){
+			public Boolean visit(Boolean element) {
+				return element;
+			}};
+		fi = new FilterInverter<Boolean>(f);
+		assertTrue(fi.visit(Boolean.FALSE));
+	}
+
+}
