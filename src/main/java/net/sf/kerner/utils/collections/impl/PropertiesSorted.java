7e4fc3738d647ec068dd56865c3da187b3cbcd01
Kerner <alex.kerner.24@googlemail.com>
2011-10-27 12:00:06 +0200

    JavaDoc.

--- src/main/java/net/sf/kerner/utils/collections/impl/PropertiesSorted.java
+++ src/main/java/net/sf/kerner/utils/collections/impl/PropertiesSorted.java
@@ -1,3 +1,18 @@
+/**********************************************************************
+Copyright (c) 2009-2011 Alexander Kerner. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ ***********************************************************************/
+
 package net.sf.kerner.utils.collections.impl;
 
 import java.util.ArrayList;
@@ -9,6 +24,24 @@ import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
+/**
+ * 
+ * An instance of {@link Properties} that keeps properties sorted by string representation of keys.
+ * 
+ * <p>
+ * <b>Example:</b><br>
+ *
+ * </p>
+ * <p>
+ * <pre>
+ * TODO example
+ * </pre>
+ * </p>
+ *
+ * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
+ * @version 2011-10-27
+ *
+ */
 public class PropertiesSorted extends Properties {
 
 	private static final long serialVersionUID = -3190096405009723488L;
