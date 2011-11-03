20a80dd86b8621b6e5ce9ea1aec3fb5f661dc557
Kerner <alex.kerner.24@googlemail.com>
2011-11-03 17:01:25 +0100

    JavaDoc.

--- src/main/java/net/sf/kerner/utils/collections/Visitor.java
+++ src/main/java/net/sf/kerner/utils/collections/Visitor.java
@@ -17,33 +17,43 @@ package net.sf.kerner.utils.collections;
 
 /**
  * 
- * <a href="http://de.wikipedia.org/wiki/Visitor">Wikipedia: Visitor Pattern</a>
+ * <a href="http://de.wikipedia.org/wiki/Visitor">Wikipedia: Visitor
+ * Pattern</a></p> A {@code Visitor} can also be seen as a <a
+ * href="http://en.wikipedia.org/wiki/Closure_%28computer_science%29"
+ * >Closure</a>, where {@link Visitor#visit(Object)} represents the closure/
+ * function, which has an input of type {@code E} and and output/ result of type
+ * {@code R}.
  * 
  * <p>
  * <b>Example:</b><br>
- *
+ * 
  * </p>
  * <p>
+ * 
  * <pre>
  * TODO example
  * </pre>
+ * 
  * </p>
- *
+ * 
  * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
  * @version 2011-10-26
- *
- * @param <R> type of result
- * @param <E> type of input object
+ * 
+ * @param <R>
+ *            type of result
+ * @param <E>
+ *            type of input
  */
 public interface Visitor<R, E> {
 
 	/**
 	 * 
 	 * Visit object {@code element}, perform any action and return result.
-	 *
-	 * @param element element to visit
+	 * 
+	 * @param element
+	 *            element to visit
 	 * @return result of visit
 	 */
 	R visit(E element);
-	
+
 }
