0d350ad6033ced753cd83da1ee6ccd0144f839b7
Kerner <alex.kerner.24@googlemail.com>
2011-11-08 11:58:16 +0100

    [API-Break] Move AbstractTransformingListFactory to list.impl.

--- src/main/java/net/sf/kerner/utils/collections/list/impl/AbstractTransformingListFactory.java
+++ src/main/java/net/sf/kerner/utils/collections/list/impl/AbstractTransformingListFactory.java
@@ -1,9 +1,9 @@
-package net.sf.kerner.utils.collections.list;
+package net.sf.kerner.utils.collections.list.impl;
 
 import java.util.Collection;
 import java.util.List;
 
-import net.sf.kerner.utils.collections.list.impl.AbstractListTransformer;
+import net.sf.kerner.utils.collections.list.ListFactory;
 
 public abstract class AbstractTransformingListFactory<T, V> extends AbstractListTransformer<T, V> implements ListFactory<V> {
 
