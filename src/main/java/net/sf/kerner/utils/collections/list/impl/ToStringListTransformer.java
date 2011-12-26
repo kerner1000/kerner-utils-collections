b6585270cc0e965b18374aeab64b8540fb543933
Kerner <alex.kerner.24@googlemail.com>
2011-12-26 17:32:38 +0100

    [Feature] ToStringListTransformer: Add optional ToStringStrategy usage.

--- src/main/java/net/sf/kerner/utils/collections/list/impl/ToStringListTransformer.java
+++ src/main/java/net/sf/kerner/utils/collections/list/impl/ToStringListTransformer.java
@@ -5,14 +5,32 @@ import java.util.Collection;
 import java.util.List;
 
 import net.sf.kerner.utils.collections.CollectionTransformer;
+import net.sf.kerner.utils.collections.ToStringStrategy;
 
 public class ToStringListTransformer implements CollectionTransformer<Object, String> {
+	
+	public final static ToStringStrategy<Object> DEFAULT_TO_STRING_STRATEGY = new ToStringStrategy<Object>() {
+
+		public String visit(Object element) {
+			return element.toString();
+		}
+	};
+	
+	private final ToStringStrategy<Object> s;
+	
+	public ToStringListTransformer() {
+		s = DEFAULT_TO_STRING_STRATEGY;
+	}
+	
+	public ToStringListTransformer(ToStringStrategy<Object> s) {
+		this.s = s;
+	}
 
 	public List<String> transformCollection(
 			Collection<? extends Object> element) {
 		final List<String> result = new ArrayList<String>();
 		for (Object o : element) {
-			result.add(o.toString());
+			result.add(s.visit(o));
 		}
 		return result;
 	}
