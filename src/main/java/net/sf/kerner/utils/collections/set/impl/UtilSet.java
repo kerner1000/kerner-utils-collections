b596a4ae55da203efa95aac61cc29fa09f04c7fc
Kerner <alexanderkerner24@gmail.com>
2013-05-31 15:25:39 +0200

    Feature: Introduce UtilSet.

---
+++ src/main/java/net/sf/kerner/utils/collections/set/impl/UtilSet.java
@@ -0,0 +1,28 @@
+package net.sf.kerner.utils.collections.set.impl;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class UtilSet {
+
+    public static <T> T getFirstElement(final Set<T> set) {
+        return set.iterator().next();
+    }
+
+    public static <T> T getLastElement(final Set<T> set) {
+        return new ArrayList<T>(set).get(set.size() - 1);
+    }
+
+    public static boolean isNullSet(final Set<?> set) {
+        if (set == null) {
+            return true;
+        }
+        for (final Object o : set) {
+            if (o != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
