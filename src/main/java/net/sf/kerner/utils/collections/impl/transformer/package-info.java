f06d58ee811efda8c5644e4414da37ad50307906
Kerner <alexanderkerner24@gmail.com>
2013-08-08 13:02:01 +0200

    Add package.

---
+++ src/main/java/net/sf/kerner/utils/collections/impl/transformer/package-info.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+/**
+ * TODO description
+ *
+ * <p>
+ * <b>Example:</b><br>
+ *
+ * </p>
+ * <p>
+ * <pre>
+ * TODO example
+ * </pre>
+ * </p>
+ * <p>
+ * last reviewed: 0000-00-00
+ * </p>
+ *
+ * @author <a href="mailto:alexanderkerner24@gmail.com">Alexander Kerner</a>
+ * @version 0000-00-00
+ *
+ */
+package net.sf.kerner.utils.collections.impl.transformer;
