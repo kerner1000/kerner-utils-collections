7a80dfa0d3e89e760aa29711610a1c7a3f368062
Kerner <alexanderkerner24@gmail.com>
2013-08-27 13:45:34 +0200

    Feature: Add MapMap#putAll().

--- src/main/java/net/sf/kerner/utils/collections/map/impl/MapMap.java
+++ src/main/java/net/sf/kerner/utils/collections/map/impl/MapMap.java
@@ -16,17 +16,100 @@ public class MapMap<T, K, V> {
 
     private final FactoryMap<K, V> factory;
 
-    public MapMap(final Map<T, Map<K, V>> map, final FactoryMap<K, V> factory) {
-        this.map = map;
-        this.factory = factory;
+    public MapMap() {
+        this(new LinkedHashMap<T, Map<K, V>>(), new FactoryLinkedHashMap<K, V>());
     }
 
     public MapMap(final Map<T, Map<K, V>> map) {
         this(map, new FactoryLinkedHashMap<K, V>());
     }
 
-    public MapMap() {
-        this(new LinkedHashMap<T, Map<K, V>>(), new FactoryLinkedHashMap<K, V>());
+    public MapMap(final Map<T, Map<K, V>> map, final FactoryMap<K, V> factory) {
+        this.map = map;
+        this.factory = factory;
+    }
+
+    public void clear() {
+        map.clear();
+    }
+
+    public boolean containsKey1(final T key) {
+        return map.containsKey(key);
+    }
+
+    public boolean containsKey2(final K key) {
+        for (final Map<K, V> v : map.values()) {
+            if (v.keySet().contains(key))
+                return true;
+        }
+        return false;
+    }
+
+    public boolean containsValue(final V value) {
+        for (final Map<K, V> v : map.values()) {
+            if (v.values().contains(value))
+                return true;
+        }
+        return false;
+    }
+
+    public Set<Entry<T, Map<K, V>>> entrySet() {
+        return map.entrySet();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        return map.equals(o);
+    }
+
+    public Map<K, V> get(final T key1) {
+        return map.get(key1);
+    }
+
+    @Override
+    public int hashCode() {
+        return map.hashCode();
+    }
+
+    /**
+     * TODO description
+     * 
+     * @return {@code true} if {{@link #size1()} == 0}; {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    public List<K> keyList2() {
+        final List<K> result = new ArrayList<K>();
+        for (final Map<K, V> v : map.values()) {
+            result.addAll(v.keySet());
+        }
+        return result;
+    }
+
+    public Set<T> keySet1() {
+        return map.keySet();
+    }
+
+    public void put(final T key1, final K key2, final V value) {
+        UtilMap.addToMapMap(map, key1, key2, value, factory);
+    }
+
+    public void putAll(final T key1, final Map<K, V> map) {
+        for (final Entry<K, V> e : map.entrySet()) {
+            put(key1, e.getKey(), e.getValue());
+        }
+    }
+
+    public Map<K, V> remove1(final T key1) {
+        return map.remove(key1);
+    }
+
+    public void remove2(final K key2) {
+        for (final Map<K, V> c : map.values()) {
+            c.remove(key2);
+        }
     }
 
     /**
@@ -51,65 +134,15 @@ public class MapMap<T, K, V> {
         return size;
     }
 
-    /**
-     * TODO description
-     * 
-     * @return {@code true} if {{@link #size1()} == 0}; {@code false} otherwise
-     */
-    public boolean isEmpty() {
-        return map.isEmpty();
+    @Override
+    public String toString() {
+        return map.toString();
     }
 
-    public boolean containsKey1(final T key) {
-        return map.containsKey(key);
-    }
-
-    public boolean containsKey2(final K key) {
-        for (final Map<K, V> v : map.values()) {
-            if (v.keySet().contains(key))
-                return true;
-        }
-        return false;
-    }
-
-    public boolean containsValue(final V value) {
-        for (final Map<K, V> v : map.values()) {
-            if (v.values().contains(value))
-                return true;
-        }
-        return false;
-    }
-
-    public Map<K, V> get(final T key1) {
-        return map.get(key1);
-    }
-
-    public void put(final T key1, final K key2, final V value) {
-        UtilMap.addToMapMap(map, key1, key2, value, factory);
-    }
-
-    public Map<K, V> remove1(final T key1) {
-        return map.remove(key1);
-    }
-
-    public void remove2(final K key2) {
+    public Collection<V> values() {
+        final Collection<V> result = new ArrayList<V>();
         for (final Map<K, V> c : map.values()) {
-            c.remove(key2);
-        }
-    }
-
-    public void clear() {
-        map.clear();
-    }
-
-    public Set<T> keySet1() {
-        return map.keySet();
-    }
-
-    public List<K> keyList2() {
-        final List<K> result = new ArrayList<K>();
-        for (final Map<K, V> v : map.values()) {
-            result.addAll(v.keySet());
+            result.addAll(c.values());
         }
         return result;
     }
@@ -123,33 +156,6 @@ public class MapMap<T, K, V> {
      */
     public Collection<V> values(final K key1) {
         return map.get(key1).values();
-    }
-
-    public Collection<V> values() {
-        final Collection<V> result = new ArrayList<V>();
-        for (final Map<K, V> c : map.values()) {
-            result.addAll(c.values());
-        }
-        return result;
-    }
-
-    public Set<Entry<T, Map<K, V>>> entrySet() {
-        return map.entrySet();
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        return map.equals(o);
-    }
-
-    @Override
-    public int hashCode() {
-        return map.hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return map.toString();
     }
 
 }
