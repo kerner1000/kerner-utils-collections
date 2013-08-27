1efb8dd6c7626d92c98dfad5b272a682a286dc04
Kerner <alexanderkerner24@gmail.com>
2013-08-27 13:40:28 +0200

    Feature: Add package frequencies.

---
+++ src/main/java/net/sf/kerner/utils/collections/frequencies/Frequencies.java
@@ -0,0 +1,7 @@
+package net.sf.kerner.utils.collections.frequencies;
+
+import java.util.Collection;
+
+public interface Frequencies<T extends Frequency> extends Collection<T> {
+
+}
---
+++ src/main/java/net/sf/kerner/utils/collections/frequencies/Frequency.java
@@ -0,0 +1,9 @@
+package net.sf.kerner.utils.collections.frequencies;
+
+public interface Frequency {
+
+    int getFrequency();
+
+    void setFrequency(int frequency);
+
+}
