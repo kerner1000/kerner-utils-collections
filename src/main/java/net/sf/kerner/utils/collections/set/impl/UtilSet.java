48967c84d1d3398dd8d897d84b691f0822740e4d
Kerner <alexanderkerner24@gmail.com>
2014-06-03 15:18:00 +0200

    API-Break: Remove UtilSet#isNullSet().

--- src/main/java/net/sf/kerner/utils/collections/set/impl/UtilSet.java
+++ src/main/java/net/sf/kerner/utils/collections/set/impl/UtilSet.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2010-2014 Alexander Kerner. All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,18 +28,6 @@ public class UtilSet {
 
     public static <T> T getLastElement(final Set<T> set) {
         return new ArrayList<T>(set).get(set.size() - 1);
-    }
-
-    public static boolean isNullSet(final Set<?> set) {
-        if (set == null) {
-            return true;
-        }
-        for (final Object o : set) {
-            if (o != null) {
-                return false;
-            }
-        }
-        return true;
     }
 
     public static <T> Set<T> newSet() {
