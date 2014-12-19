70e40487e9ab989e75cd0affa7abc0ec9e9e5cf1
Kerner <alexanderkerner24@gmail.com>
2014-12-19 13:29:33 +0100

    ComparatorInverter: Make debuggable.

--- src/main/java/net/sf/kerner/utils/collections/ComparatorInverter.java
+++ src/main/java/net/sf/kerner/utils/collections/ComparatorInverter.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2010-2014 Alexander Kerner. All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,13 +25,13 @@ import net.sf.kerner.utils.Util;
  * <b>Example:</b><br>
  * </p>
  * <p>
- * 
+ *
  * <pre>
  * TODO example
  * </pre>
- * 
+ *
  * </p>
- * 
+ *
  * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
  * @version 2012-05-23
  * @see Comparator
@@ -48,7 +48,8 @@ public class ComparatorInverter<T> implements Comparator<T> {
     }
 
     public int compare(final T o1, final T o2) {
-        return c.compare(o2, o1);
+        final int result = c.compare(o2, o1);
+        return result;
     }
 
 }
