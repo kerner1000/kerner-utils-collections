617a373561583428a8e71b7c0608cdb6713534d4
Alexander Kerner <ak@silico-sciences.com>
2015-10-10 17:09:05 +0200

    API-Break: Move TransformerIteratorToEnumeration to collections.transformer.

--- src/test/java/net/sf/kerner/utils/collections/impl/TestIteratorToEnumerationTransformer.java
+++ src/test/java/net/sf/kerner/utils/collections/impl/TestIteratorToEnumerationTransformer.java
@@ -20,7 +20,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.Enumeration;
 
-import net.sf.kerner.utils.collections.TransformerIteratorToEnumeration;
+import net.sf.kerner.utils.collections.trasformer.TransformerIteratorToEnumeration;
 
 import org.junit.After;
 import org.junit.AfterClass;
