f085df31375453e1cc14d56767db60af248be7a7
Alexander Kerner <ak@silico-sciences.com>
2015-12-03 15:31:05 +0100

    Stuff.

--- src/main/java/net/sf/kerner/utils/collections/visitor/VisitorApplierProto.java
+++ src/main/java/net/sf/kerner/utils/collections/visitor/VisitorApplierProto.java
@@ -1,4 +1,5 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * Copyright (c) 2010-2015 Alexander Kerner. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,35 +13,32 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ *****************************************************************************
+ */
 package net.sf.kerner.utils.collections.visitor;
 
 import java.util.Collection;
-
 import net.sf.kerner.utils.collections.UtilCollection;
 import net.sf.kerner.utils.visitor.Visitor;
 import net.sf.kerner.utils.visitor.VisitorApplier;
 
 public class VisitorApplierProto<E> implements VisitorApplier<E> {
 
-	protected final Collection<Visitor<E>> visitors = UtilCollection
-			.newCollection();
+    protected final Collection<Visitor<E>> visitors = UtilCollection
+            .newCollection();
 
-	public synchronized void addVisitor(final Visitor<E> visitor) {
-		visitors.add(visitor);
-	}
+    public synchronized void addVisitor(final Visitor<E> visitor) {
+        visitors.add(visitor);
+    }
 
-	public synchronized void clearVisitors() {
-		visitors.clear();
-	}
+    public synchronized void clearVisitors() {
+        visitors.clear();
+    }
 
-	public Void transform(final E e) {
-
-		for (final Visitor<E> v : visitors) {
-			v.transform(e);
-		}
-		return null;
-
-	};
-
+    public synchronized Void transform(final E e) {
+        for (final Visitor<E> v : visitors) {
+            v.transform(e);
+        }
+        return null;
+    }
 }
